658c67dde9450381bbf766f2223db2a8f388b083
Gregor Lenz <mail@lenzgregor.com>
2023-09-03 11:23:32 +0300

    bump version to 1.0.4

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name = "importRosbag",
-    version = "1.0.3",
+    version = "1.0.4",
     author = "Sim Bamford",
     description = ("Standalone rosbag loader for python3"),
     packages=['importRosbag', 'importRosbag.messageTypes'],
